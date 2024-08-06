ba4b06e5b2b55ec75946a26c0cc6e2771405a4aa
Pravin Bansal (PT) <p.bansal@premiertutors.com>
2024-08-06 13:24:23 +1000

    Restoration of modularity

--- sliders/pt_slick_slider.js
+++ sliders/pt_slick_slider.js
@@ -1,82 +1,122 @@
-// Function to load an external script
-function loadScript(url, callback) {
+// Load the Slick carousel library
+(function loadSlickLibrary() {
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = url;
-    script.onload = callback;
+    script.src = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
+    script.onload = initializeCarousels;
     document.head.appendChild(script);
-  }
+  })();
   
-  // Load the Slick carousel library and then run the custom script
-  loadScript('//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', function() {
-    // Your custom JavaScript code below
-
-  // Carousel
-  $(window).on('resize', function(){
-    resizeArrows();
-  });
-
-  function resizeArrows() {
-    var height = $($('.tutor-image')[0]).css('height');
-    var parsedHeight = parseInt(height, 10);
-
-    $('.slick-arrow').each(function() {
-      $(this).css('top', (parsedHeight/2 - 15) + 'px');
+  // Core function with default settings
+  function initializeCarousel(options) {
+    var defaultSettings = {
+      targetDiv: '.carousel',
+      cmsItem: '.tutor-item',
+      responsiveSettings: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            arrows: true
+          }
+        },
+        {
+          breakpoint: 250,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ],
+      dots: false,
+      accessibility: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 600,
+      infinite: true,
+      arrows: true,
+      prevArrow: "<img class='slick-prev' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/607cd7bce2745da838252992_chevron-left.svg'>",
+      nextArrow: "<img class='slick-next' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/601cb92b2fe85c3dc00650dc_chevron-right.svg'>"
+    };
+  
+    // Merge default settings with custom options
+    var settings = { ...defaultSettings, ...options };
+  
+    $(window).on('resize', function() {
+      resizeArrows(settings.targetDiv, settings.cmsItem);
+    });
+  
+    $(settings.targetDiv).slick({
+      dots: settings.dots,
+      accessibility: settings.accessibility,
+      autoplay: settings.autoplay,
+      autoplaySpeed: settings.autoplaySpeed,
+      speed: settings.speed,
+      infinite: settings.infinite,
+      arrows: settings.arrows,
+      appendArrows: settings.targetDiv,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      prevArrow: settings.prevArrow,
+      nextArrow: settings.nextArrow,
+      appendArrows: $(settings.targetDiv),
+      appendDots: $(settings.targetDiv),
+      responsive: settings.responsiveSettings
+    });
+  
+    $(settings.cmsItem).each(function(index, element) {
+      var tutorItem = $(this).children();
+      var div = tutorItem[0];
+  
+      $(settings.targetDiv).slick('slickAdd', div);
+  
+      var isLastElement = index == $(settings.cmsItem).length - 1;
+      if (isLastElement) resizeArrows(settings.targetDiv, settings.cmsItem);
     });
   }
   
-  // Carousel code below
-
-  let panel = document.getElementById('pane_1');
-  let slickLeftButton = "<img class='slick-prev' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/607cd7bce2745da838252992_chevron-left.svg'>";
-  let slickRightButton = "<img class='slick-next' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/601cb92b2fe85c3dc00650dc_chevron-right.svg'>";
-
-$('.carousel').slick({
-    dots: false,
-    accessibility: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    speed: 600, //changed from 600
-    infinite: true,
-    arrows: true,
-    appendArrows: panel,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    prevArrow: slickLeftButton,
-    nextArrow: slickRightButton,
-    appendArrows: $('.carousel'),
-    appendDots: $('.carousel'),
-    responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 2,
-        arrows: true
-      }
-    },
-    {
-      breakpoint: 250, //478
-      settings: {
-        slidesToShow: 1
-      }
-    }
-  ]
-  });
- 
-
-  $('.tutor-item').each(function(index, element) {
-    var tutorItem = $(this).children(); // .categories value (the cms option selected)
-    var div = tutorItem[0];
-
-    $('.carousel').slick('slickAdd', div);
-
-    var isLastElement = index == $('.tutor-item').length -1;
-    if (isLastElement) resizeArrows();
-  }); 
-});
+  // Function to resize arrows
+  function resizeArrows(targetDiv, cmsItem) {
+    var height = $($(targetDiv).find('.tutor-image')[0]).css('height');
+    var parsedHeight = parseInt(height, 10);
+  
+    $(targetDiv).find('.slick-arrow').each(function() {
+      $(this).css('top', (parsedHeight / 2 - 15) + 'px');
+    });
+  }
+  
+  // Initialize multiple carousels with different settings
+  function initializeCarousels() {
+    // Carousel 1 - Could be done with just initializeCarousel() but for demonstration purposes have used full settings here:
+    initializeCarousel({
+      targetDiv: '.carousel',
+      cmsItem: '.tutor-item',
+      dots: true,
+      autoplay: false,
+      responsiveSettings: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+}
