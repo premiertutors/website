05c1bc8c05f6df21198829c5ee4073dc33dada9b
Pravin Bansal (PT) <p.bansal@premiertutors.com>
2024-08-06 13:09:59 +1000

    Reverting to old

--- sliders/pt_slick_slider.js
+++ sliders/pt_slick_slider.js
@@ -3,69 +3,72 @@
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
-    script.onload = initializeCarousels;
+    script.onload = initializeCarousel;
     document.head.appendChild(script);
-})();
-
-function initializeCarousel(targetDiv, cmsItem) {
-    var $targetDiv = $(targetDiv);
-    var $cmsItems = $(cmsItem);
-
-    // Initialize Slick
-    $targetDiv.slick({
-        dots: false,
-        arrows: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        prevArrow: "<img class='slick-prev' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/607cd7bce2745da838252992_chevron-left.svg'>",
-        nextArrow: "<img class='slick-next' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/601cb92b2fe85c3dc00650dc_chevron-right.svg'>",
-        responsive: [
-            {
-                breakpoint: 991,
-                settings: {
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 250,
-                settings: {
-                    slidesToShow: 1
-                }
-            }
-        ]
+  })();
+  
+  // Carousel functions
+  function resizeArrows() {
+    var height = $($('.tutor-image')[0]).css('height');
+    var parsedHeight = parseInt(height, 10);
+  
+    $('.slick-arrow').each(function() {
+      $(this).css('top', (parsedHeight/2 - 15) + 'px');
     });
-
-    // Add CMS items to the carousel
-    $cmsItems.each(function() {
-        var $item = $(this).children().first();
-        $targetDiv.slick('slickAdd', $item);
-    });
-
-    // Resize arrows
-    function resizeArrows() {
-        var height = $targetDiv.find('.tutor-image').first().height();
-        $targetDiv.find('.slick-arrow').css('top', (height / 2 - 15) + 'px');
-    }
-
-    // Call resizeArrows on window resize and after initialization
+  }
+  
+  function initializeCarousel() {
     $(window).on('resize', resizeArrows);
-    resizeArrows();
-}
-
-function initializeCarousels() {
-    // Initialize carousels here
-    initializeCarousel('.carousel', '.tutor-item');
-    // You can add more carousels here, e.g.:
-    // initializeCarousel('.another-carousel', '.another-cms-item');
-}
-
-// Initialize when the DOM is ready
-document.addEventListener('DOMContentLoaded', initializeCarousels);
+  
+    let panel = document.getElementById('pane_1');
+    let slickLeftButton = "<img class='slick-prev' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/607cd7bce2745da838252992_chevron-left.svg'>";
+    let slickRightButton = "<img class='slick-next' src='https://uploads-ssl.webflow.com/5fe828cda66535ed122d1912/601cb92b2fe85c3dc00650dc_chevron-right.svg'>";
+  
+    $('.carousel').slick({
+      dots: false,
+      accessibility: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 600,
+      infinite: true,
+      arrows: true,
+      appendArrows: panel,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      prevArrow: slickLeftButton,
+      nextArrow: slickRightButton,
+      appendArrows: $('.carousel'),
+      appendDots: $('.carousel'),
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            arrows: true
+          }
+        },
+        {
+          breakpoint: 250,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  
+    $('.tutor-item').each(function(index, element) {
+      var tutorItem = $(this).children();
+      var div = tutorItem[0];
+  
+      $('.carousel').slick('slickAdd', div);
+  
+      var isLastElement = index == $('.tutor-item').length - 1;
+      if (isLastElement) resizeArrows();
+    });
+  }
